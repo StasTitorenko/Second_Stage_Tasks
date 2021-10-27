23f8f6e6f88052c24eb5772a0a0c4b74b776653a
Stas Titorenko <fanta2000@tut.by>
2021-10-27 08:59:12 +0300

    add abstract page

--- seleniumWebDriver/src/test/java/iCanWin/pages/PastebinAddNewPastePage.java
+++ seleniumWebDriver/src/test/java/iCanWin/pages/PastebinAddNewPastePage.java
@@ -2,22 +2,17 @@ package iCanWin.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class PastebinAddNewPastePage {
-    private final WebDriver driver;
-    private final WebDriverWait wait;
-
+public class PastebinAddNewPastePage extends AbstractPage {
     @FindBy(xpath = "//*[@class='textarea']")
     WebElement resultData;
 
-    public PastebinAddNewPastePage(WebDriver driver, WebDriverWait wait) {
-        this.driver = driver;
-        this.wait = wait;
-        PageFactory.initElements(driver, this);
+    public PastebinAddNewPastePage(WebDriver driver, WebDriverWait wait, Actions actions) {
+        super(driver, wait, actions);
     }
 
     public String checkOfCreateNewPaste() {
--- seleniumWebDriver/src/test/java/iCanWin/pages/PastebinHomePage.java
+++ seleniumWebDriver/src/test/java/iCanWin/pages/PastebinHomePage.java
@@ -4,17 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class PastebinHomePage {
+public class PastebinHomePage extends AbstractPage {
     private static final String HOMEPAGE_URL = "https://pastebin.com";
     private static final String PASTE = "Hello from WebDriver";
     private static final String PASTE_NAME = "helloweb";
-    private final WebDriver driver;
-    private final WebDriverWait wait;
-    private final Actions actions;
 
     @FindBy(id = "postform-text")
     private WebElement textField;
@@ -32,10 +28,7 @@ public class PastebinHomePage {
     private WebElement btnCreateNewPaste;
 
     public PastebinHomePage(WebDriver driver, WebDriverWait wait, Actions actions) {
-        this.driver = driver;
-        this.wait = wait;
-        this.actions = actions;
-        PageFactory.initElements(driver, this);
+        super(driver, wait, actions);
     }
 
     public PastebinHomePage openPage() {
@@ -64,6 +57,6 @@ public class PastebinHomePage {
 
     public PastebinAddNewPastePage createNewPaste() {
         btnCreateNewPaste.submit();
-        return new PastebinAddNewPastePage(driver, wait);
+        return new PastebinAddNewPastePage(driver, wait, actions);
     }
 }
