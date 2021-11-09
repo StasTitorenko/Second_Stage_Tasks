1bd65ff30f28cb3e9ed55e8021e6ffb7bc68e8b7
Stas Titorenko <fanta2000@tut.by>
2021-11-09 10:11:08 +0300

    change test messages

--- framework/src/test/java/framework/tests/CorrectFillingCalculatorFormTest.java
+++ framework/src/test/java/framework/tests/CorrectFillingCalculatorFormTest.java
@@ -9,62 +9,62 @@ public class CorrectFillingCalculatorFormTest extends CommonConditions {
     public void checkTabFilling() {
         Assert.assertEquals(calculatorResultsPage.getTab(),
                 calculator.getTab().toString(),
-                "Tabs are not equal");
+                "Actual tab is not equal to Expected ");
     }
 
     @Test(description = "Checking correctness of filling Number of Instances")
     public void checkNumberOfInstancesFilling() {
         Assert.assertEquals(calculatorResultsPage.getNumberOfInstances(),
                 formatFields.formatNumberOfInstances(calculator.getNumberOfInstances()),
-                "Number of instances are not equal");
+                "Actual Number is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling Operation System")
     public void checkOperationSystemFilling() {
         Assert.assertEquals(calculatorResultsPage.getOperationSystem(),
                 formatFields.formatOperationSystem(calculator.getOperationSystem()),
-                "Operation systems are not equal");
+                "Actual Operation System is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling Machine Class")
     public void checkMachineClassFilling() {
         Assert.assertEquals(calculatorResultsPage.getMachineClass(),
                 formatFields.formatMachineClass(calculator.getMachineClass()),
-                "Operation systems are not equal");
+                "Actual Machine Class is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of fillingMachine Type")
     public void checkMachineTypeFilling() {
         Assert.assertEquals(calculatorResultsPage.getMachineType(),
                 formatFields.formatMachineType(calculator.getMachineType()),
-                "Machine types are not equal");
+                "Actual Machine Type is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling GPU Number and Type")
     public void checkGPUTypeFilling() {
         Assert.assertEquals(calculatorResultsPage.getGPU(),
                 formatFields.formatGPU(calculator.getNumberOfGPU(), calculator.getGpuType()),
-                "GPU are not equal");
+                "Actual GPU is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling SSD number")
     public void checkSSDNumberFilling() {
         Assert.assertEquals(calculatorResultsPage.getSSD(),
                 formatFields.formatSSD(calculator.getNumberOfSSD()),
-                "SSD are not equal");
+                "Actual SSD is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling Data center")
     public void checkDataCenterFilling() {
         Assert.assertEquals(calculatorResultsPage.getDataCenter(),
                 formatFields.formatDataCenter(calculator.getDataCenter()),
-                "Data center's are not equal");
+                "Actual Data Center is not equal to Expected");
     }
 
     @Test(description = "Checking correctness of filling Committed usage")
     public void checkCommittedUsageFilling() {
         Assert.assertEquals(calculatorResultsPage.getCommitmentUsage(),
                 formatFields.formatCommittedUsage(calculator.getCommittedUsage()),
-                "Committed usages are not equal");
+                "Actual Committed Usage is not equal to Expected");
     }
 }
